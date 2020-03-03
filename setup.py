5348e7a3470c977d4310c272318e5f80d058d43d
Shane Breeze <s.breeze15@imperial.ac.uk>
2020-03-03 12:28:42 +0000

    Add setup scripts

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "README.md"), 'r') as f:
+    long_description = f.read()
+
+setup(
+    name="qstats",
+    version="0.0.1",
+    author="Shane Breeze",
+    author_email="sdb15@ic.ac.uk",
+    description="SGE qstat info",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shane-breeze/qstats",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
